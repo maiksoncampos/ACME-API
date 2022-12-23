396f37748c0f23c7f8a1cde7795bc9b7900e7895
maiksoncampos <maikson.campos@gmail.com>
2022-12-22 21:27:59 -0300

    Estrutura incial com Identity configurado

---
+++ src/Services/ACME.Identidade.API/Program.cs
@@ -0,0 +1,22 @@
+using ACME.Identidade.API.Configuration;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddIdentityConfig(builder.Configuration);
+builder.Services.AddApiConfig(); builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfig();
+
+var app = builder.Build();
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+app.UseApiConfig(app.Environment);
+app.UseSwaggerConfig(apiVersionDescriptionProvider);
+
+app.Run();
